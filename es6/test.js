f1f99bd563d1d6371d81029a395341207635c4e9
zjjfly <zjjblue@gmail.com>
2016-09-10 11:06:23 +0800

    add eslint config and gulp-eslint

--- es6/test.js
+++ es6/test.js
@@ -1,18 +1,19 @@
 /**
  * Created by zjjfly on 16/9/10.
  */
-'use strict';
+"use strict";
 //es6新特性：块
-const sentences=[
-    {subject:'Javascript',verb: "is",object:'great'},
-    {subject:'Elephants',verb: "are",object:'large'},
-]
+const sentences = [
+    {subject: "Javascript", verb: "is", object: "great"},
+    {subject: "Elephants", verb: "are", object: "large"},
+];
 //es6新特性：对象解构
-function say({subject,verb,object}) {
-    console.log('${subject} ${verb} ${object}');
+function say({subject, verb, object}) {
+    console.log(`${subject} ${verb} ${object}`);
 }
 //es6特性：for...of
-for(let s of sentences){
+for (let s of sentences) {
     say(s);
 }
+//es特性：箭头函数(lambda)
 sentences.map(o=>say(o));
--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
-const gulp=require('gulp');
-const babel=require('gulp-babel');
-gulp.task('default',function () {
+const gulp = require("gulp");
+const babel = require("gulp-babel");
+const eslint=require("gulp-eslint");
+gulp.task("default", function () {
     gulp.src("es6/**/*.js")
         .pipe(babel())
         .pipe(gulp.dest("dist"));
     gulp.src("public/es6/**/*.js")
         .pipe(babel())
         .pipe(gulp.dest("public/dist"));
-})
+    // Run ESLint
+    gulp.src(["es6/**/*.js", "public/es6/**/*.js"])
+        .pipe(eslint())
+        .pipe(eslint.format());
+});

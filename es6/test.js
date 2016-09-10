a1f803ad6f24c27117abca118de7745ec90d3a82
zjjfly <zjjblue@gmail.com>
2016-09-10 09:44:18 +0800

    add gulp,bable;test es6 transpiler

---
+++ es6/test.js
@@ -0,0 +1,18 @@
+/**
+ * Created by zjjfly on 16/9/10.
+ */
+'use strict';
+//es6新特性：块
+const sentences=[
+    {subject:'Javascript',verb: "is",object:'great'},
+    {subject:'Elephants',verb: "are",object:'large'},
+]
+//es6新特性：对象解构
+function say({subject,verb,object}) {
+    console.log('${subject} ${verb} ${object}');
+}
+//es6特性：for...of
+for(let s of sentences){
+    say(s);
+}
+sentences.map(o=>say(o));

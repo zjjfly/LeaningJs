33f01d31650811f740dfdea6807a7a5196f8efe3
zjjfly <zjjblue@gmail.com>
2016-09-14 16:34:33 +0800

    add control flow code

---
+++ es6/chapter4/CrownAndAnchor.js
@@ -0,0 +1,58 @@
+/**
+ * Created by zjjfly on 16/9/14.
+ */
+"use strict";
+let funds = 50;
+let round = 0;
+while (funds > 1 && funds < 100) {
+    console.log(`round ${round}:`);
+    console.log(`\tstarting funds: ${funds}p`);
+    const bets = {crown: 0, anchor: 0, heart: 0, spade: 0, club: 0, diamond: 0};
+    let totalBet = rand(1, funds);
+    if (new Date().getDay()==3){
+        totalBet=1;
+    } else if (totalBet === 7) {
+        totalBet = funds;
+        bets.heart = totalBet;
+    } else {
+        let remain = totalBet;
+        do {
+            let bet = rand(1, remain);
+            let face = randFace();
+            bets[face] += bet;
+            remain -= bet;
+        } while (remain > 0);
+    }
+    console.log("\tbet:" + Object.keys(bets).map(face=>`${face}: ${bets[face]} pence`).join(",") + `tatal: ${totalBet} pence`);
+    funds = funds - totalBet;
+    const hand = roll();
+    console.log(`\thand: ${hand.join(",")}`);
+    let winnings = 0;
+    hand.filter(face=>bets[face] > 0).forEach(face=>winnings += bets[face]);//使用es6的箭头函数简化下面的for循环
+    // for (var i = 0; i < hand.length; i++) {
+    //     var face = hand[i];
+    //     if (bets[face] > 0) {
+    //         winnings += bets[face];
+    //     }
+    // }
+    funds += winnings;
+    round++;
+    console.log("\twinnings:" + winnings);
+}
+console.log(`\tending funs: ${funds}`);
+
+function rand(m, n) {
+    return m + Math.floor((n - m + 1) * Math.random());
+}
+
+function randFace() {
+    return ["crown", "anchor", "heart", "spade", "club", "diamond"][rand(0, 5)];
+}
+
+function roll() {
+    let hand = [];
+    for (var roll = 0; roll < 3; roll++) {
+        hand.push(randFace());
+    }
+    return hand;
+}
---
+++ es6/chapter4/Others.js
@@ -0,0 +1,36 @@
+/**
+ * Created by zjjfly on 16/9/14.
+ */
+"use strict";
+//可以利用逗号在for循环的第一部分多重赋值，还可以在第三部分进行多个操作
+for (let temp, i = 0, j = 1; j < 30; temp = i, i = j, j = i + temp)
+    console.log(j);
+// for(;;) console.log("I will repeat forever");//无限循环
+switch (1) {
+    case 1:
+        console.log("true");
+        break;
+    default:
+        console.log("default");
+        break;
+}
+//for in 循环
+const player = {name: "Thomas", rank: "Midshipman", age: 25};
+for (let prop in player) {
+    if (!player.hasOwnProperty(prop)) continue;
+    console.log(prop + ":" + player[prop]);
+}
+//es6新的语法 for of循环
+const keys = Object.keys(player);
+for (let prop of keys) {
+    console.log(prop + ":" + player[prop]);
+}
+//如果要在遍历数组的时候删除其中的符合某些条件的元素，需要倒序遍历
+const bigArrayOfNumbers = [1, 2, 3, 4, 5, 6];
+for (let i = bigArrayOfNumbers.length - 1; i >= 0; i--) {
+    if (isPrime(bigArrayOfNumbers[i])) bigArrayOfNumbers.splice(i, 1);
+}
+console.log(bigArrayOfNumbers);
+function isPrime(i) {
+    return i == 1;
+}

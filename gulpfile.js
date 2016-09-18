f0fee282cf352bcefaf03e6e682feea7e46d9398
zjjfly <zjjblue@gmail.com>
2016-09-18 14:05:48 +0800

    enable eslint to return error

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const gulp = require("gulp");
 const babel = require("gulp-babel");
-const eslint=require("gulp-eslint");
+const eslint = require("gulp-eslint");
 gulp.task("default", function () {
     gulp.src("es6/**/*.js")
         .pipe(babel())
@@ -11,5 +11,6 @@ gulp.task("default", function () {
     // Run ESLint
     gulp.src(["es6/**/*.js", "public/es6/**/*.js"])
         .pipe(eslint())
-        .pipe(eslint.format());
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
 });

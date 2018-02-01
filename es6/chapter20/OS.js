096d790fec712bf54b50aca6e4ae176b0dbc6332
zjjfly <zjjblue@gmail.com>
2018-02-01 09:19:36 +0800

    调整计算内存的准确性

--- es6/chapter20/OS.js
+++ es6/chapter20/OS.js
@@ -1,18 +1,19 @@
 /**
  * Created by zjjfly on 2016/10/13.
  */
-"use strict";
+'use strict';
 //Node核心模块 OS
-const os = require("os");
+const os = require('os');
 
-console.log("Hostname: " + os.hostname());
-console.log("OS type: " + os.type());
-console.log("OS platform: " + os.platform());
-console.log("OS release: " + os.release());
-console.log("OS uptime: " +
-    (os.uptime() / 60 / 60 / 24).toFixed(1) + " days");
-console.log("CPU architecture: " + os.arch());
-console.log("Number of CPUs: " + os.cpus().length);
-console.log("Total memory: " + (os.totalmem() / 1e6).toFixed(1) + " MB");
+console.log('Hostname: ' + os.hostname());
+console.log('OS type: ' + os.type());
+console.log('OS platform: ' + os.platform());
+console.log('OS release: ' + os.release());
+console.log('OS uptime: ' +
+    (os.uptime() / 60 / 60 / 24).toFixed(1) + ' days');
+console.log('CPU architecture: ' + os.arch());
+console.log('Number of CPUs: ' + os.cpus().length);
+const OneMB=1024*1024;
+console.log('Total memory: ' + (os.totalmem() / OneMB).toFixed(1) + ' MB');
 // 1042.3 MB
-console.log("Free memory: " + (os.freemem() / 1e6).toFixed(1) + " MB");
+console.log('Free memory: ' + (os.freemem() / OneMB).toFixed(1) + ' MB');

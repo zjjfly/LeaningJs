c681fe7f36cc99a72dbd73ed4e8dcefb0249f9e5
zjjfly <zjjblue@gmail.com>
2018-02-12 01:47:20 +0800

    优化自动编译代码

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,48 @@
+//@ts-check
 const gulp = require('gulp');
-const path = require('path');
+const Path = require('path');
+const fs = require('fs');
 const babel = require('gulp-babel');
 const eslint = require('gulp-eslint');
 const plumber = require('gulp-plumber');
 const del = require('del');
 
 gulp.task('eslint', function () {
-    return gulp.src(['es6/**/*.js', 'public/es6/**/*.js'])
+    return gulp.src(['es6/**/*.js'])
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
 });
 gulp.task('autoCompile', function () {
-    gulp.watch('es6/**/*.js', function (event) {
-        console.log('File ' + event.path + ' was ' + event.type + '.');
-        if ('changed' === event.type || 'added' === event.type) {
-            compile(event.path);
-        }
-        if ('deleted' === event.type) {
-            var filePathFromSrc = path.relative(path.resolve('es6'), event.path);
-            var destFilePath = path.resolve('dist', filePathFromSrc);
+    //first,compile all file existed
+    gulp.src(['es6/**/*.js'])
+        .pipe(babel())
+        .pipe(gulp.dest('dist'));
+    gulp.watch('es6/**', function (event) {
+        const ext = Path.extname(event.path);
+        const filePathFromSrc = Path.relative(Path.resolve('es6'), event.path);
+        const destFilePath = Path.resolve('dist', filePathFromSrc);
+        if ('deleted' !== event.type) {
+            if (ext === '.js') {
+                compile(event.path, filePathFromSrc, destFilePath);
+            } else if (ext === '' && event.type === 'added') {
+                fs.mkdir(destFilePath, err => {
+                    if (err) {
+                        console.error(err);
+                    }
+                });
+            }
+        } else {
             del.sync(destFilePath);
         }
     });
 });
 
-function compile(file) {
-    var filePathFromSrc = path.relative(path.resolve('es6'), file);
-    var destFilePath = path.dirname(path.resolve('dist', filePathFromSrc));
-    console.log(destFilePath);
+function compile(file, srcPath, destPath) {
+    console.log(`compile ${Path.basename(file)} ,compiled file:dist/${srcPath}`);
     gulp.src(file).pipe(plumber(function (error) {
         console.log('compile error:\n' + error);
     }))
         .pipe(babel())
-        .pipe(gulp.dest(destFilePath));
+        .pipe(gulp.dest(Path.dirname((destPath))));
 }
